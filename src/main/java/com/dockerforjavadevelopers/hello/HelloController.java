11c3244a1bd427e2de9dcf14fd69dab4011226b8
mohangarimella <mohangarimella@gmail.com>
2020-06-16 15:32:05 -0400

    Update HelloController.java

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello Capgemini World\n";
+        return "Hello Capgeminis World\n";
     }
     
 }
