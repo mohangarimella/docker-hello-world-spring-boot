65b441901f31ceb4474564d010c8104bd6e60c46
mohangarimella <mohangarimella@gmail.com>
2020-06-17 18:22:39 -0400

    Update HelloController.java

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "<H1><center><p style='color:red'>This is Capgemini's DevOps solution for Kubernetes world\n</p></center></H1>";
+        return "<H1><center><p style='color:red'>This is Capgemini's DevOps solution for AWS Kubernetes world\n</p></center></H1>";
     }
     
 }
